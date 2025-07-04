f2a8cdb7766cd34dc98c1d8cb0bbc34cbaca5235
Ghassan <112765677+ghassanelgendy@users.noreply.github.com>
2025-07-05 01:56:31 +0300

    Change Version Trial

--- Services/UpdateConfig.cs
+++ Services/UpdateConfig.cs
@@ -3,14 +3,14 @@ namespace chronos_screentime.Services
     public static class UpdateConfig
     {
         // Current version of the application
-        public const string CurrentVersion = "1.1.7";
+        public const string CurrentVersion = "1.1.8";
         
         // GitHub repository information
         public const string RepositoryOwner = "ghassanelgendy";
         public const string RepositoryName = "chronos-screentime";
         
         // Update URLs - change these when you release new versions
-        public const string LatestVersion = "v1.1.7";
+        public const string LatestVersion = "v1.1.8";
         public static string ManifestUrl => $"https://github.com/{RepositoryOwner}/{RepositoryName}/releases/download/{LatestVersion}/manifest.json";
         
         // Update check interval (24 hours)
